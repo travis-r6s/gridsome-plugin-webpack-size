cebd462271412d06ff22824c352ac4f52f4416bf
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-14 17:18:09 +0000

    Add development and file options

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const SizePlugin = require('size-plugin')
-module.exports = api => {
+module.exports = (api, options) => {
   api.chainWebpack( config => {
     if (process.env.NODE_ENV === 'production') {
-      const options = { writeFile: false }
+      config.plugin('size').use(SizePlugin).init((Plugin) => new Plugin(options))
+    }
+
+    if ( process.env.NODE_ENV === 'development' && options.development ) {
       config.plugin('size').use(SizePlugin).init((Plugin) => new Plugin(options))
     }
   })
 }
+
+module.exports.defaultOptions = () => ( {
+  writeFile: false,
+  development: false
+})
